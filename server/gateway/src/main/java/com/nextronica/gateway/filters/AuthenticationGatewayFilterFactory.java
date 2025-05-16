4e27f412c415ed0ec94c3af3d337684c6be9e005
Eslam Hawas <eslam.sabry@intalio.com>
2025-05-17 00:48:36 +0300

    Comment out `AuthenticationGatewayFilterFactory` implementation.
    The entire `AuthenticationGatewayFilterFactory` class has been commented out, effectively disabling its functionality. This is likely a temporary measure. Ensure to restore or replace the functionality if required in the future.

--- server/gateway/src/main/java/com/nextronica/gateway/filters/AuthenticationGatewayFilterFactory.java
+++ server/gateway/src/main/java/com/nextronica/gateway/filters/AuthenticationGatewayFilterFactory.java
@@ -1,71 +1,71 @@
-package com.nextronica.gateway.filters;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.nextronica.gateway.utils.JwtUtil;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.gateway.filter.GatewayFilter;
-import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.server.reactive.ServerHttpResponse;
-import org.springframework.stereotype.Component;
-import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Mono;
-
-import java.nio.charset.StandardCharsets;
-import java.time.Instant;
-import java.util.HashMap;
-import java.util.Map;
-
-@Component
-public class AuthenticationGatewayFilterFactory extends AbstractGatewayFilterFactory<Object> {
-
-    private final JwtUtil jwtUtil;
-
-    @Autowired
-    public AuthenticationGatewayFilterFactory(JwtUtil jwtUtil) {
-        super(Object.class);
-        this.jwtUtil = jwtUtil;
-    }
-
-    @Override
-    public GatewayFilter apply(Object config) {
-        return (exchange, chain) -> {
-
-            if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
-                return onError(exchange, "Authorization header missing", HttpStatus.FORBIDDEN);
-            }
-
-            String token = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
-            if (token == null || !jwtUtil.validateToken(token)) {
-                return onError(exchange, "Invalid token", HttpStatus.UNAUTHORIZED);
-            }
-
-            return chain.filter(exchange);
-        };
-    }
-
-    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus status) {
-        ServerHttpResponse response = exchange.getResponse();
-        response.setStatusCode(status);
-
-        response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
-
-        Map<String, Object> errorAttributes = new HashMap<>();
-        errorAttributes.put("timestamp", Instant.now().toString());
-        errorAttributes.put("error", err);
-        errorAttributes.put("path", exchange.getRequest().getPath().value());
-        try {
-            byte[] bytes = new ObjectMapper().writeValueAsBytes(errorAttributes);
-            DataBuffer buffer = response.bufferFactory().wrap(bytes);
-            return response.writeWith(Mono.just(buffer));
-        } catch (JsonProcessingException e) {
-            byte[] bytes = "{\"error\":\"Internal server error\"}".getBytes(StandardCharsets.UTF_8);
-            DataBuffer buffer = response.bufferFactory().wrap(bytes);
-            return response.writeWith(Mono.just(buffer));
-        }
-    }
-}
+//package com.nextronica.gateway.filters;
+//
+//import com.fasterxml.jackson.core.JsonProcessingException;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.nextronica.gateway.utils.JwtUtil;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.cloud.gateway.filter.GatewayFilter;
+//import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+//import org.springframework.core.io.buffer.DataBuffer;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.MediaType;
+//import org.springframework.http.server.reactive.ServerHttpResponse;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.server.ServerWebExchange;
+//import reactor.core.publisher.Mono;
+//
+//import java.nio.charset.StandardCharsets;
+//import java.time.Instant;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@Component
+//public class AuthenticationGatewayFilterFactory extends AbstractGatewayFilterFactory<Object> {
+//
+//    private final JwtUtil jwtUtil;
+//
+//    @Autowired
+//    public AuthenticationGatewayFilterFactory(JwtUtil jwtUtil) {
+//        super(Object.class);
+//        this.jwtUtil = jwtUtil;
+//    }
+//
+//    @Override
+//    public GatewayFilter apply(Object config) {
+//        return (exchange, chain) -> {
+//
+//            if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+//                return onError(exchange, "Authorization header missing", HttpStatus.FORBIDDEN);
+//            }
+//
+//            String token = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+//            if (token == null || !jwtUtil.validateToken(token)) {
+//                return onError(exchange, "Invalid token", HttpStatus.UNAUTHORIZED);
+//            }
+//
+//            return chain.filter(exchange);
+//        };
+//    }
+//
+//    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus status) {
+//        ServerHttpResponse response = exchange.getResponse();
+//        response.setStatusCode(status);
+//
+//        response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
+//
+//        Map<String, Object> errorAttributes = new HashMap<>();
+//        errorAttributes.put("timestamp", Instant.now().toString());
+//        errorAttributes.put("error", err);
+//        errorAttributes.put("path", exchange.getRequest().getPath().value());
+//        try {
+//            byte[] bytes = new ObjectMapper().writeValueAsBytes(errorAttributes);
+//            DataBuffer buffer = response.bufferFactory().wrap(bytes);
+//            return response.writeWith(Mono.just(buffer));
+//        } catch (JsonProcessingException e) {
+//            byte[] bytes = "{\"error\":\"Internal server error\"}".getBytes(StandardCharsets.UTF_8);
+//            DataBuffer buffer = response.bufferFactory().wrap(bytes);
+//            return response.writeWith(Mono.just(buffer));
+//        }
+//    }
+//}
