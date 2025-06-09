4e42a4da394a45a9e08d418da4347e78e876acfb
Eslam Hawas <eslam.sabry@intalio.com>
2025-06-09 14:54:26 +0300

    feat: Add Profit Margin Module

---
+++ server/server/src/main/java/com/nextronica/server/models/ProfitMargin.java
@@ -0,0 +1,39 @@
+package com.nextronica.server.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "profit-margin")
+public class ProfitMargin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @NotNull
+    @ColumnDefault("current_timestamp(6)")
+    @Column(name = "createdDate", nullable = false)
+    private LocalDateTime createdDate;
+
+    @Column(name = "endDate")
+    private LocalDateTime endDate;
+
+    @Column(name = "commissionPercentage", precision = 5, scale = 2)
+    private BigDecimal commissionPercentage;
+
+
+    @PrePersist
+    protected void onCreate() {
+        createdDate = LocalDateTime.now();
+    }
+
+}
