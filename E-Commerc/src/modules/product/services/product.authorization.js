37d607af1e6072af9fea691cdb4e37806ab116f4
Eslam Hawas <eslam.sabry@intalio.com>
2025-06-13 19:36:35 +0300

    feat: Update workspace configuration for Cart Management API and related services

--- E-Commerc/src/modules/product/services/product.authorization.js
+++
@@ -1,7 +0,0 @@
-export const endPoint = {
-    editUserProduct:['vendor','admin'],
-    createProduct: ['vendor', 'admin'],
-    updateProduct: ['admin'],
-    deleteProduct: ['admin'],
-    joinProductAsVendor: ['vendor'],
-}
--- E-Commerc/src/modules/user/services/user.validation.js
+++
@@ -1,5 +0,0 @@
-import joi from 'joi';
-
-export const createUser = joi.object().keys({
-    name: joi.string().min(3).max(50).trim().required(),
-}).required();
